bd99cea73fe5c2d956e049714a6934c42f8f5484
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-02-09 11:57:29 +0530

    Fect(local): Added Necessery files required for local data management

---
+++ app/src/main/java/com/example/aditya/gojek/data/local/ContentProvider.java
@@ -0,0 +1,8 @@
+package com.example.aditya.gojek.data.local;
+
+/**
+ * Created by Aditya on 09-Feb-17.
+ */
+
+public class ContentProvider {
+}
---
+++ app/src/main/java/com/example/aditya/gojek/data/model/Contact.java
@@ -0,0 +1,8 @@
+package com.example.aditya.gojek.data.model;
+
+/**
+ * Created by Aditya on 09-Feb-17.
+ */
+
+public class Contact {
+}
