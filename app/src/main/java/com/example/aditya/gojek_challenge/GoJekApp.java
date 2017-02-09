ecf555b7f2ce30641658e5be4ae33d736b01a18e
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-02-09 11:34:09 +0530

    Fect(App): Added Application file and packages for modularity

---
+++ app/src/main/java/com/example/aditya/gojek_challenge/GoJekApp.java
@@ -0,0 +1,22 @@
+package com.example.aditya.gojek_challenge;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+import timber.log.Timber;
+
+/**
+ * Created by Aditya on 09-Feb-17.
+ */
+
+public class GoJekApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+            Stetho.initializeWithDefaults(this);
+        }
+    }
+}
--- app/src/main/java/com/example/aditya/gojek_challenge/ui/MainActivity.java
+++ app/src/main/java/com/example/aditya/gojek_challenge/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.aditya.gojek_challenge;
+package com.example.aditya.gojek_challenge.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.aditya.gojek_challenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
