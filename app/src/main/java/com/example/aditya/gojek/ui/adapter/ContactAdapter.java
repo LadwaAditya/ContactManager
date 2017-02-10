1b061c8a23cc6d45d63e2c51a31cf93a842b1fdd
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-02-10 16:56:20 +0530

    Fect(ContactAdapter): Changed Contact Adapter to use data binding

--- app/src/main/java/com/example/aditya/gojek/ui/adapter/ContactAdapter.java
+++ app/src/main/java/com/example/aditya/gojek/ui/adapter/ContactAdapter.java
@@ -3,7 +3,9 @@ package com.example.aditya.gojek.ui.adapter;
 import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.aditya.gojek.R;
 import com.example.aditya.gojek.data.model.Contact;
@@ -11,12 +13,14 @@ import com.example.aditya.gojek.databinding.ListItemContactBinding;
 
 import java.util.ArrayList;
 
+import timber.log.Timber;
+
 /**
+ * An Adapter that holds contact info
  * Created by Aditya on 09-Feb-17.
  */
 
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder> {
-
 
     private ArrayList<Contact> contactArrayList;
 
@@ -32,23 +36,29 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactV
 
     @Override public void onBindViewHolder(ContactViewHolder holder, int position) {
         final Contact contact = contactArrayList.get(position);
-        holder.setContact(contact);
+        holder.bindContact(contact);
     }
 
     @Override public int getItemCount() {
         return contactArrayList != null ? contactArrayList.size() : 0;
     }
 
-    static class ContactViewHolder extends RecyclerView.ViewHolder {
+    public static class ContactViewHolder extends RecyclerView.ViewHolder {
         private ListItemContactBinding mBinding;
-
-        public void setContact(Contact contact) {
-            mBinding.txtContactName.setText(contact.getFirstName() + " " + contact.getLastName());
-        }
 
         public ContactViewHolder(ListItemContactBinding mBinding) {
             super(mBinding.getRoot());
             this.mBinding = mBinding;
+            this.mBinding.setHolder(this);
+        }
+
+        public void bindContact(Contact contact) {
+            mBinding.setContact(contact);
+        }
+
+        public void onClick(View view) {
+            Timber.d("Clicked");
+            Toast.makeText(view.getContext(), String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
         }
     }
 }
