bee1bb5ddd4d49f6242cd40443738ef4015509fc
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-02-11 18:14:43 +0530

    Fect(ContactDetailViewModel): Added SMS, Call and Email features
    Added Send sms
    Added call phone directly and handle permission using RxPermission
    Added Email feature

--- app/src/main/java/com/example/aditya/gojek/ui/detail/ContactDetailViewModel.java
+++ app/src/main/java/com/example/aditya/gojek/ui/detail/ContactDetailViewModel.java
@@ -1,13 +1,20 @@
 package com.example.aditya.gojek.ui.detail;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
+import android.net.Uri;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.devdoo.rxpermissions.RxPermission;
 import com.example.aditya.gojek.R;
 import com.example.aditya.gojek.data.model.Contact;
 
@@ -65,6 +72,47 @@ public class ContactDetailViewModel extends BaseObservable {
                 .error(R.mipmap.round)
                 .bitmapTransform(new CropCircleTransformation(imageView.getContext()))
                 .into(imageView);
+    }
+
+    public void onClickSendMessage(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("sms:" + getPhone()));
+        if (intent.resolveActivity(mActivity.getPackageManager()) != null)
+            mActivity.startActivity(Intent.createChooser(intent, mActivity.getString(R.string.intent_chooser_title_sms)));
+        else
+            Toast.makeText(mActivity, R.string.no_activity_sms, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickShareContact(View view) {
+        Context context = view.getContext();
+        Toast.makeText(context, mContact.getFirstName(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSendEmail(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{getEmail()});
+        if (intent.resolveActivity(mActivity.getPackageManager()) != null)
+            mActivity.startActivity(Intent.createChooser(intent, mActivity.getString(R.string.intent_chooser_title_email)));
+        else
+            Toast.makeText(mActivity, R.string.no_activity_email, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void onClickCallPhone(View view) {
+        RxPermission.with(mActivity.getFragmentManager()).request(Manifest.permission.CALL_PHONE).subscribe(granted -> {
+            if (granted) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + getPhone()));
+                if (intent.resolveActivity(mActivity.getPackageManager()) != null)
+                    mActivity.startActivity(intent);
+                else
+                    Toast.makeText(mActivity, R.string.no_activity_call, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(mActivity, R.string.permission_call, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
