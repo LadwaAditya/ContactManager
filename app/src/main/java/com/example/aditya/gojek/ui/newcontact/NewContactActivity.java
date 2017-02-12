1aa9fa6c036ff00b6cfc5d06094f43236267196f
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-02-12 14:41:38 +0530

    Fect(NewContact): Addded Validation logic

--- app/src/main/java/com/example/aditya/gojek/ui/newcontact/NewContactActivity.java
+++ app/src/main/java/com/example/aditya/gojek/ui/newcontact/NewContactActivity.java
@@ -7,6 +7,7 @@ import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Toast;
@@ -63,17 +64,48 @@ public class NewContactActivity extends BaseActivity implements NewContactContra
                                             startActivityForResult(cameraIntent, REQUEST_CODE_CAPTURE_IMAGE);
                                             break;
                                         case 1:
-                                            Intent galleryIntent = new Intent(
-                                                    Intent.ACTION_PICK,
-                                                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                            Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                                             startActivityForResult(galleryIntent, REQUEST_CODE_GALLERY);
                                             break;
                                     }
                                 }).create().show();
                     } else {
-                        Toast.makeText(this, "Need Camera permission", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, R.string.camara_permission, Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+
+    public void onClickSaveContact(View view) {
+        boolean name = true, phone = true;
+        String firstName = mBinding.included.txtFirstName.getText().toString();
+        String mobileNumber = mBinding.included.txtMobileNumber.getText().toString();
+
+        if (firstName.length() < 3) {
+            name = false;
+            Snackbar.make(mBinding.coordinatorLayout, "First Name not valid", Snackbar.LENGTH_LONG)
+                    .show();
+        }
+        if (mobileNumber.length() >= 12 && mobileNumber.length() <= 15) {
+            if (mobileNumber.length() == 15) {
+                boolean flag;
+                flag = Character.toString(mobileNumber.charAt(0)).equalsIgnoreCase("+");
+                if (!flag) {
+                    phone = false;
+                    Snackbar.make(mBinding.coordinatorLayout, "Number not valid", Snackbar.LENGTH_LONG)
+                            .show();
+                }
+            }
+        } else {
+            phone = false;
+            Snackbar.make(mBinding.coordinatorLayout, "Number not valid", Snackbar.LENGTH_LONG)
+                    .show();
+        }
+
+        if (phone && name) {
+            //Make rest call
+            Toast.makeText(this, "Fields are valid", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
